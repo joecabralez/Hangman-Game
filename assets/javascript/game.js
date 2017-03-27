acf193521dd1c825e7e80143e0ac8550b2e3523c
Joe <cabralez07@gmail.com>
2017-03-26 21:25:03 -0500

    updated 3.26

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,203 +1,110 @@
 var game = {
-
-		wins: 0,
-
-		guesses: 15,
-
-		blank_array: [],
-
-		array_of_used_letters: [],
-
-		changing_guesses: "<p style='font-size: 20px'>Guesses left: </p>",
-
-		array_of_words: ["jerry","elaine","costanza","kramer","newman", "seinfeld"],
-
-		words_used: [],
-
-		random_index: 
-			function(){
-				return Math.floor(Math.random() * (this.array_of_words.length));
-			},
-
-		word_chooser: 
-			function(){
-				return game.array_of_words[game.random_index()];
-			},
-
-		blank_space_filler: 
-			function(word,array){
-
-			 	for(var i = 0; i < word.length; i++){
-			 		array.push(" _ ");
-				}
-			 	return array;
-		 	},
-
-		contains: 
-			function(a, obj) {
-			    for (var i = 0; i < a.length; i++) {
-			        if (a[i] === obj) {
-			            return true;
-			        }
-			    }
-			    return false;
+	wins: 0,
+	guesses: 15,
+	emptyArray: [],
+	usedLetters: [],
+	remainingGuesses: "<p style='font-size: 20px'>Guesses left: </p>",
+	wordsArray: ["Jerry", "Elaine", "Costanza", "Kramer", "Newman", "Seinfeld"],
+	usedWords: [],
+	randomIndex: function() {
+		return Math.floor(Math.random() * (this.wordsArray.length));
+	},
+	wordChooser: function() {
+		return game.wordsArray[game.randomIndex()];
+	},
+	blankSpaces: function(word, array) {
+		for (var i = 0; i < word.length; i++) {
+			array.push(" _ ");
+		}
+		return array;
+	},
+	contains: function(a, obj) {
+		for (var i = 0; i < a.length; i++) {
+			if (a[i] === obj) {
+				return true;
 			}
-
+		}
+		return false;
 	}
-
-	var chosen_word = game.word_chooser();
-
-	// need  to print a " _ " for each letter of the word chosen
-
-	var blank_word = game.blank_space_filler(chosen_word,game.blank_array);
-
-	document.querySelector("#current_word").innerHTML = blank_word.join("");
-
- 	// print how many guesses the user has since the beginning
-
-	document.querySelector("#guesses_remaining_title").innerHTML = game.changing_guesses;
-
-
-	document.querySelector("#guesses_remaining_number").innerHTML = game.guesses;
-
-	document.onkeyup = function(event) {
-
-		var key_up = {
-
-			changing_word: blank_word,
-
-			user_guess: event.key,
-
-			letter_title: "<p style='font-size: 30px'>You have already used: </p>",
-
-			print_wins: function(){document.querySelector("#wins").innerHTML = "Wins: " + game.wins},
-			print_letters_used: function(){document.querySelector("#letters_used").innerHTML = game.array_of_used_letters.join(", ")},
-			print_blank_word: function(){document.querySelector("#current_word").innerHTML = blank_word.join("")},
-			print_guesses_number: function(){document.querySelector("#guesses_remaining_number").innerHTML = game.guesses},
-
-			boo: new Audio("boo.mp3"),
-
-			cheer: new Audio("win.mp3")
-
-		}
-
-		// check if the letter typed by the user is in the chosen word
-
-		// if it is not and that letter has not been picked before, just update letters used and number of guesses available
-
-
-		if(game.contains(game.array_of_used_letters,key_up.user_guess) !== true){
-			game.array_of_used_letters.push(key_up.user_guess);
-			game.guesses = game.guesses - 1;
-		}
-
-		// if it is, then put the letter in its corresponding space 
-	
-		if(game.contains(chosen_word,key_up.user_guess)){
-			for(var i = 0; i < chosen_word.length; i++)  {
-
-				if(chosen_word[i] === key_up.user_guess){
-				key_up.changing_word[i] = " " + key_up.user_guess + " ";
-				document.querySelector("#current_word").innerHTML = key_up.changing_word.join("");
-				}
-
-			}
-				
-		}
-		
-		// update game and print guesses, letters used, etc
-
-		key_up.print_guesses_number();
-
-		document.querySelector("#letters_used_title").innerHTML = key_up.letter_title;
-
-		key_up.print_letters_used();
-
-		// game is won when there are no more spaces to fill. We reset everything and choose a new word
-
-		if(game.contains(key_up.changing_word," _ ") === false){
-
-			alert("you won!!! You guessed: " + chosen_word);
-			
-			// if word was guessed, it goes into the "words_used" array so it does not get repeated
-
-			game.words_used.push(chosen_word);
-
-			game.wins = game.wins +1;
-
-			//game ends when the user guesses all 6 words
-
-			if(game.wins === 6){
-				alert("You went through all the words!! game will restart");
-				game.words_used = [];
-				game.wins = 0;
-			}
-
-			// we display the image of the soccer player whose name was guessed
-
-			document.querySelector("#victoryImage").innerHTML = "<img class='col-xs-12' src='assets/images/" + chosen_word + ".jpg' + alt=" + chosen_word + ">";
-
-
-			chosen_word = game.word_chooser();
-
-			while(game.contains(game.words_used,chosen_word)){
-				chosen_word = game.word_chooser();
-			}
-
-			console.log(game.words_used);
-
-			// for next word we reset guesses, array to put " _ ", array with used letters, and create a new "blank_word" (a string with "_" instead of the chosen word's letters)
-
-			game.guesses = 15;
-
-			game.blank_array = [];
-
-			game.array_of_used_letters = [];
-
-			blank_word = game.blank_space_filler(chosen_word,game.blank_array);
-
-			// print updated wins, letters used, new blank word and number of guesses
-
-			key_up.print_wins();
-			key_up.print_letters_used();
-			key_up.print_blank_word();
-			key_up.print_guesses_number();
-
-			// play cheering audio
-			key_up.cheer.play();
-
-		}
-
-		// if user runs out of guesses
-
-		if(game.guesses === 0){
-
-			alert("you lost!!!");
-
-			chosen_word = game.word_chooser();
-
-			while(game.contains(game.words_used,chosen_word)){
-				chosen_word = game.word_chooser();
-			}
-
-			// for next word we reset guesses, array to put " _ ", array with used letters, and create a new "blank_word" (a string with "_" instead of the chosen word's letters)
-			
-			game.guesses = 15;
-
-			game.blank_array = [];
-
-			game.array_of_used_letters = [];
-
-			blank_word = game.blank_space_filler(chosen_word,game.blank_array);
-			
-			key_up.print_letters_used();
-			key_up.print_blank_word();
-			key_up.print_guesses_number();
-			
-			// play loser's audio
-			key_up.boo.play();
-
-
-		}
-
+}
+var chosenWord = game.wordChooser();
+var blankWord = game.blankSpaces(chosenWord, game.emptyArray);
+document.querySelector("#currentWord").innerHTML = blankWord.join("");
+document.querySelector("#remainingGuessesTitle").innerHTML = game.remainingGuesses;
+document.querySelector("#remainingGuessesNumber").innerHTML = game.guesses;
+document.onkeyup = function(event) {
+	var keyUp = {
+		changingWord: blankWord,
+		userGuess: event.key,
+		letterTitle: "<p style='font-size: 20px'>You've used: </p>",
+		print_wins: function() {
+			document.querySelector("#wins").innerHTML = "Wins: " + game.wins
+		},
+		print_letters_used: function() {
+			document.querySelector("#lettersUsed").innerHTML = game.usedLetters.join(", ")
+		},
+		print_blank_word: function() {
+			document.querySelector("#currentWord").innerHTML = blankWord.join("")
+		},
+		print_guesses_number: function() {
+			document.querySelector("#remainingGuessesNumber").innerHTML = game.guesses
+		},
+		boo: new Audio("assets/images/crowdboo.mp3"),
+		applause: new Audio("assets/images/applause.mp3")
 	}
+	if (game.contains(game.usedLetters, keyUp.userGuess) !== true) {
+		game.usedLetters.push(keyUp.userGuess);
+		game.guesses = game.guesses - 1;
+	}
+	if (game.contains(chosenWord, keyUp.userGuess)) {
+		for (var i = 0; i < chosenWord.length; i++) {
+			if (chosenWord[i] === keyUp.userGuess) {
+				keyUp.changingWord[i] = " " + keyUp.userGuess + " ";
+				document.querySelector("#currentWord").innerHTML = keyUp.changingWord.join("");
+			}
+		}
+	}
+	keyUp.print_guesses_number();
+	document.querySelector("#lettersUsedTitle").innerHTML = keyUp.letterTitle;
+	keyUp.print_letters_used();
+	if (game.contains(keyUp.changingWord, " _ ") === false) {
+		alert("Helloooo you're a winner! You guessed: " + chosenWord);
+		game.usedWords.push(chosenWord);
+		game.wins = game.wins + 1;
+		if (game.wins === 6) {
+			alert("You're a Seinfeld master - play again!");
+			game.usedWords = [];
+			game.wins = 0;
+		}
+		document.querySelector("#victoryImage").innerHTML = "<img class='col-xs-12' src='assets/images/" + chosenWord + ".jpg' + alt=" + chosenWord + ">";
+		chosenWord = game.wordChooser();
+		while (game.contains(game.usedWords, chosenWord)) {
+			chosenWord = game.wordChooser();
+		}
+		console.log(game.usedWords);
+		game.guesses = 15;
+		game.emptyArray = [];
+		game.usedLetters = [];
+		blankWord = game.blankSpaces(chosenWord, game.emptyArray);
+		keyUp.print_wins();
+		keyUp.print_letters_used();
+		keyUp.print_blank_word();
+		keyUp.print_guesses_number();
+		keyUp.cheer.play();
+	}
+	if (game.guesses === 0) {
+		alert("No wins for you!!");
+		chosenWord = game.wordChooser();
+		while (game.contains(game.usedWords, chosenWord)) {
+			chosenWord = game.wordChooser();
+		}
+		game.guesses = 15;
+		game.emptyArray = [];
+		game.usedLetters = [];
+		blankWord = game.blankSpaces(chosenWord, game.emptyArray);
+		keyUp.print_letters_used();
+		keyUp.print_blank_word();
+		keyUp.print_guesses_number();
+		keyUp.boo.play();
+	}
+}
